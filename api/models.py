141ff6a47fb2f1c8c2063d4e42604dda0e5be27f
Oliver Wright <meiamsome@meiamso.me>
2017-01-28 14:15:02 +0000

    Implement LazyEnum
    Implement Device and Sensor classes

--- api/models.py
+++ api/models.py
@@ -1,27 +1,86 @@
 from django.db import models
 from django.conf import settings
 
+
+class LazyEnum(object):
+    class Value(object):
+        def __init__(self, value, name, *args, **kwargs):
+            super(LazyEnum.Value, self).__init__(*args, **kwargs)
+            self.value = value
+            self.name = name
+
+        def __len__(self):
+            return 2
+
+        def __getitem__(self, key):
+            return (self.value, self.name)[key]
+
+        def __iter__(self):
+            return iter((self.value, self.name))
+
+        def __str__(self):
+            return self.name
+
+        def __repr__(self):
+            return "Value({}, {})".format(self.value, self.name)
+
+    def __init__(self, *values, **kwargs):
+        super(LazyEnum, self).__init__(**kwargs)
+        self.values = tuple(LazyEnum.Value(i, name) for i, name in enumerate(values))
+        for value in self.values:
+            setattr(self, str(value).upper(), value)
+
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, key):
+        return self.values[key]
+
+    def __iter__(self):
+        return iter(self.values)
+
+    def from_id(self, idx):
+        return self.values[idx]
+
+    def __repr__(self):
+        return "LazyEnum({})".format(", ".join(repr(enumerate(self.values))))
+
 # Create your models here.
 class Haus(models.Model):
     name = models.CharField(max_length=200)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="owned_hauses")
     users = models.ManyToManyField(settings.AUTH_USER_MODEL, through="UAC")
 
-class Levels(object):
-    _levels = ["Owner", "Admin", "Resident", "Landlord", "Guest"]
-
-    def __init__(self, number, *args, **kwargs):
-        super(Levels, self).__init__(*args, **kwargs)
-        self.number = number
-
-    def __str__(self):
-        return Levels._levels[self.number]
-
 
 class UAC(models.Model):
+    LEVELS = LazyEnum("Owner", "Admin", "Resident", "Landlord", "Guest")
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     haus = models.ForeignKey(Haus)
-    level = models.PositiveSmallIntegerField(choices=((i, str(Levels(i))) for i in range(5)))
+    level = models.PositiveSmallIntegerField(choices=LEVELS)
 
-for i, name in enumerate(Levels._levels):
-    setattr(UAC, name.upper(), Levels(i))
+class Device(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    last_ping = models.DateTimeField(db_index=True, blank=True, null=True)
+    haus = models.ForeignKey(Haus, blank=True, null=True)
+    setup_secret = models.CharField(max_length=256)
+
+class Sensor(models.Model):
+    CATEGORIES = LazyEnum(
+        "Other",
+        "Temperature",
+        "PIR",
+        "Gyroscope",
+        "Moisture",
+        "Light",
+        "Smoke",
+        "Molecule Detector",
+        "Radiation",
+    )
+    device = models.ForeignKey(Device)
+    name = models.CharField(max_length=200)
+    category = models.PositiveSmallIntegerField(choices=CATEGORIES)
+    last_datum = models.TextField()
+    formatter = models.TextField()
+    file_store = models.FileField(blank=True, null=True)
