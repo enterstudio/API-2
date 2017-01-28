80a50815f5d9414f586d6ef556878410309d9cee
Callum Mann <cm13558@my.bristol.ac.uk>
2017-01-28 17:31:06 +0000

    move model serialization and routing into views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,75 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from django.contrib.auth.models import User
+from api.models import Haus, Device, Sensor, UAC
+from rest_framework import serializers, viewsets, routers
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class HausSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Haus
+        fields = ('name', 'owner', 'users')
+
+
+# ViewSets define the view behavior.
+class HausViewSet(viewsets.ModelViewSet):
+    queryset = Haus.objects.all()
+    serializer_class = HausSerializer
+
+
+class DeviceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('uuid', 'name', 'last_ping', 'haus')
+
+
+# ViewSets define the view behavior.
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+
+class SensorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ('device', 'name', 'category', 'last_datum')
+
+
+# ViewSets define the view behavior.
+class SensorViewSet(viewsets.ModelViewSet):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'haus', HausViewSet)
+router.register(r'device', DeviceViewSet)
+router.register(r'sensor', SensorViewSet)
 
 # Create your views here.
--- lazyhausapi/urls.py
+++ lazyhausapi/urls.py
@@ -1,65 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from django.contrib import admin
-from api.models import Haus, Device, Sensor, UAC
-from rest_framework import serializers, viewsets, routers
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'is_staff')
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class HausSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Haus
-        fields = ('name', 'owner', 'users')
-
-
-# ViewSets define the view behavior.
-class HausViewSet(viewsets.ModelViewSet):
-    queryset = Haus.objects.all()
-    serializer_class = HausSerializer
-
-
-class DeviceSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('uuid', 'name', 'last_ping', 'haus')
-
-
-# ViewSets define the view behavior.
-class DeviceViewSet(viewsets.ModelViewSet):
-    queryset = Device.objects.all()
-    serializer_class = DeviceSerializer
-
-
-class SensorSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Sensor
-        fields = ('device', 'name', 'category', 'last_datum')
-
-
-# ViewSets define the view behavior.
-class SensorViewSet(viewsets.ModelViewSet):
-    queryset = Sensor.objects.all()
-    serializer_class = SensorSerializer
-
-
-# Routers provide a way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'haus', HausViewSet)
-router.register(r'device', DeviceViewSet)
-router.register(r'sensor', SensorViewSet)
+from api.views import router
 
 
 # Wire up our API using automatic URL routing.
