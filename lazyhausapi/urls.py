663346f7c923e7d065711b1c8f92176b70247d64
Callum Mann <cm13558@my.bristol.ac.uk>
2017-01-29 00:32:52 +0000

    added the accounts tab to urls, not fully implemented

--- lazyhausapi/urls.py
+++ lazyhausapi/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^accounts/', include('allauth.urls')),
 ] + api.views.urlpatterns
