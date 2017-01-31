75f33dd44f51309ece31fca4b581748894a7340b
Callum Mann <cm13558@my.bristol.ac.uk>
2017-01-31 15:20:50 +0000

    must be admin to view lists

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework import viewsets, routers
 from rest_framework import generics
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.permissions import IsAdminUser
 
 from .models import Haus, Device, Sensor, UAC
 from .access import UnlimitedAccess
@@ -22,16 +23,10 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (UnlimitedAccess,)
-
-
 class HausList(generics.ListCreateAPIView):
     queryset = Haus.objects.all()
     serializer_class = HausSerializer
-    permission_classes = (UnlimitedAccess,)
+    permission_classes = (IsAdminUser,)
 
 
 class HausDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -43,7 +38,7 @@ class HausDetail(generics.RetrieveUpdateDestroyAPIView):
 class DeviceList(generics.ListCreateAPIView):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
-    permission_classes = (UnlimitedAccess,)
+    permission_classes = (IsAdminUser,)
 
 
 class DeviceDetailByUUID(generics.RetrieveUpdateDestroyAPIView):
@@ -67,7 +62,7 @@ class DeviceListByHaus(generics.ListAPIView):
 class SensorList(generics.ListCreateAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
-    permission_classes = (UnlimitedAccess,)
+    permission_classes = (IsAdminUser,)
 
 
 class SensorListByDevice(generics.ListAPIView):
@@ -90,7 +85,7 @@ class SensorById(generics.RetrieveUpdateDestroyAPIView):
 class UACList(generics.ListCreateAPIView):
     queryset = UAC.objects.all()
     serializer_class = UACSerializer
-    permission_classes = (UnlimitedAccess,)
+    permission_classes = (IsAdminUser,)
 
 
 class UACDetail(generics.RetrieveUpdateDestroyAPIView):
