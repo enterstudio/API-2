2231990542039d0486fa89bdf76fc62f548bca6d
Callum Mann <cm13558@my.bristol.ac.uk>
2017-02-01 14:47:44 +0000

    Improved rest testing code with request assertion builder pattern

--- api/test_rest.py
+++ api/test_rest.py
@@ -14,64 +14,92 @@ def create_user(username, email, pw, is_staff):
     return user
 
 
-def assert_get(obj, urls, assert_status):
-    for url in urls:
-        response = obj.client.get(url, {}, format='json')
-        obj.assertEqual(response.status_code, assert_status)
+class RequestAssertion(object):
+    def __init__(self, client, url='/', method='GET', status=200, data={}):
+        self.client = client
+        self.url = url
+        self.method = method
+        self.status = status
+        self.data = data
+
+    def update(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return self
+
+    def execute(self):
+        if self.method == 'POST':
+            response = self.client.post(self.url, self.data, format='json')
+        elif self.method == 'GET':
+            response = self.client.get(self.url, self.data, format='json')
+        else:
+            raise NotImplementedError
+        assert response.status_code == self.status, 'Status mismatch'
+        return self
 
 
-def create_admin_and_user():
-    admin = create_user('Bojangle', 'bojang@heav.en', 'pass', True)
-    user = create_user('straycat', 'stray@cat.com', 'pass', False)
-    return admin, user
+class LazyAPITestBase(APITestCase):
+
+    def create_admin_and_user(self):
+        admin = create_user('Bojangle', 'bojang@heav.en', 'pass', True)
+        user = create_user('straycat', 'stray@cat.com', 'pass', False)
+        return admin, user
+
+    def login_user(self):
+        return self.client.login(username='straycat', password='pass')
+
+    def login_admin(self):
+        return self.client.login(username='Bojangle', password='pass')
 
 
-class GeneralTests(APITestCase):
+class GeneralTests(LazyAPITestBase):
     def test_create_haus(self):
         """
         Ensure we can create a new account object.
         """
         url = reverse('haus-lcdapi')
-        admin = create_user('god', 'got@heav.en', 'pass', True)
+        admin, _ = self.create_admin_and_user()
+        self.login_admin()
+
         data = {
             'name': 'TestysHaus',
             'owner': admin.id,
         }
-        self.client.login(username='god', password='pass')
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        RequestAssertion(self.client, url, "POST",
+                         status.HTTP_201_CREATED, data).execute()
+
         self.assertEqual(Haus.objects.count(), 1)
         self.assertEqual(Haus.objects.get().name, 'TestysHaus')
         return Haus.objects.get().id
 
 
-class PermissionTests(APITestCase):
+class PermissionTests(LazyAPITestBase):
     def test_lcdapi_admin_only(self):
-        admin, user = create_admin_and_user()
+        admin, _ = self.create_admin_and_user()
 
-        self.client.login(username='Bojangle', password='pass')
-        lcdapi_urls = [reverse(x + "-lcdapi")
-                       for x in ["haus", "sensor", "device", "uac"]]
-        assert_get(self, lcdapi_urls, status.HTTP_200_OK)
-        self.client.login(username='straycat', password='pass')
-        assert_get(self, lcdapi_urls, status.HTTP_403_FORBIDDEN)
-        self.client.logout()
+        self.login_admin()
+        urls = [reverse(x + "-lcdapi")
+                for x in ["haus", "sensor", "device", "uac"]]
+
+        ra = RequestAssertion(self.client, method="GET", status=200)
+        for url in urls:
+            ra.update(url=url).execute()
+        self.login_user()
+        ra.update(status=status.HTTP_403_FORBIDDEN)
+        for url in urls:
+            ra.update(url=url).execute()
 
     def test_haus_permissions(self):
-        admin, user = create_admin_and_user()
+        admin, user = self.create_admin_and_user()
 
         haus = Haus.objects.create_haus(name="Bojangle's Crib", owner=admin)
         url = reverse('haus-detail', args=[haus.id])
 
-        self.client.login(username='straycat', password='pass')
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
+        self.login_user()
+        ra = RequestAssertion(self.client, url=url, method="GET",
+                              status=status.HTTP_403_FORBIDDEN).execute()
         UAC.objects.create_uac(user, haus, 0)
+        ra.update(status=status.HTTP_200_OK).execute()
 
-        # Now user should have permission to view the haus
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
         return admin, haus
 
     def test_device_permissions(self):
@@ -81,21 +109,15 @@ class PermissionTests(APITestCase):
         empty = Haus.objects.create_haus(name="Empty Crib", owner=admin)
         nohausd = Device.objects.create_device(name="NoHausDevice", haus=empty)
 
-        url = reverse('device-detail', args=[superd.uuid])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        url = reverse('device-by-haus', args=[haus.id])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        url = reverse('device-by-haus', args=[empty.id])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        url = reverse('device-detail', args=[nohausd.uuid])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        ra = RequestAssertion(self.client).update(
+            url=reverse('device-detail', args=[superd.uuid]), status=200
+        ).execute().update(
+            url=reverse('device-by-haus', args=[haus.id]), status=200
+        ).execute().update(
+            url=reverse('device-by-haus', args=[empty.id]), status=403
+        ).execute().update(
+            url=reverse('device-detail', args=[nohausd.uuid]), status=403
+        ).execute()
 
         return superd, nohausd
 
@@ -104,18 +126,16 @@ class PermissionTests(APITestCase):
         sensor = Sensor.objects.create_sensor(superd, "1", 0)
         sensor_403 = Sensor.objects.create_sensor(nohausd, "2", 0)
 
-        url = reverse('sensor-detail', args=[sensor.id])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        ra = RequestAssertion(self.client).update(
+            url=reverse('sensor-detail', args=[sensor.id]), status=200
+        ).execute().update(
+            url=reverse('sensor-by-device', args=[superd.uuid]), status=200
+        ).execute().update(
+            url=reverse('sensor-by-device', args=[nohausd.uuid]), status=403
+        ).execute().update(
+            url=reverse('sensor-detail', args=[sensor_403.id]), status=403
+        ).execute()
 
-        url = reverse('sensor-by-device', args=[superd.uuid])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        url = reverse('sensor-by-device', args=[nohausd.uuid])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        url = reverse('sensor-detail', args=[sensor_403.id])
-        response = self.client.get(url, {}, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_the_tests(self):
+        ra = RequestAssertion(self.client, status=403, method="RAIS")
+        self.assertRaises(NotImplementedError, ra.execute)
